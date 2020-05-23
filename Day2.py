f2b5f863225a19ede2bd85d29460eef1583f863e
benjivrik <31964614+benjivrik@users.noreply.github.com>
2020-05-22 21:07:56 -0400

    Day1 - Day1

---
+++ Day2.py
@@ -0,0 +1,72 @@
+# ---------------------------------------------------
+# Author    :  Benjamin Kataliko Viranga
+# Community :  Stunt Business
+# Community website : www.stuntbusiness.com
+#
+# 30 Days - Q&A Python Basic
+# Day 2 : 23-05-2020
+# Day 2 | IG : https://www.instagram.com/benjivrik/
+# Subject :  Data Type
+#----------------------------------------------------
+# what would be the output of this program ?
+
+'''
+
+    This is  also a comment.
+
+    The function type(x) return the data type of the element x.
+
+    We can have dict, str, int , float, list, tuple, complex
+
+    Google and search by yourself as well |  I may be missing something
+
+    here.
+
+    Ex:  type('hhah') will output <class 'str'>
+
+    Let's focus on these data types : Integer , Float, String, complex
+
+'''
+
+
+#variables
+x = 'a'
+y = 1
+z = 2.5
+w = 1+2j
+
+print("----> first")
+
+print(x) #line1
+print(y) #line2
+print(z) #line3
+print(w) #line4
+
+print("----> second")
+
+print(type(x)) #line5
+print(type(y)) #line6
+print(type(z)) #line7
+print(type(w)) #line8
+
+
+print("----> third")
+
+x = '5'
+y = 1
+z = 2.5+0j
+w = 1+2j
+
+print(type(int(x))) #line9
+print(type(str(y))) #line10
+print(type(z))      #line11
+print(type(str(w))) #line12
+
+
+
+
+
+
+
+
+
