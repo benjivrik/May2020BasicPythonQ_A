fbbbb15bc3c074ebb9f8ea5cc1282899008119ad
benjivrik <31964614+benjivrik@users.noreply.github.com>
2020-06-03 21:02:50 -0400

    Day14

--- Day14.py
+++ Day14.py
@@ -13,13 +13,16 @@
 '''
 
     It is possible to graphically vizualise any set 
-    of data ('graphically vizualizable') in your program.
+    of data ('graphically visualizable') in your program.
 
-    The module matplotlib.pyplot allows you to vizualize your data 
-    through plot.
+    The module matplotlib.pyplot allows you to visualize your data 
+    through graphs.
 
-    Let's collect five delays in our program as previously shown in 
-    Day 13 and plot the value with collect.
+    Let's collect five values of the execution speed between blocks in our
+    program as previously shown in Day 13 and plot the collected values.
+
+    The result is meaningless for now.
+    Take the time to analyze the available functions.
 
 '''
 
@@ -28,19 +31,19 @@ import time
 
 
 
-your_I_to_II = int(time.time()*1000)
+your_I = int(time.time()*1000)
 
 print("BLOCK I_to_II")
 print("Adding a random line.")
 
-your_II_to_III = int(time.time()*1000)
+your_II = int(time.time()*1000)
 
 print("BLOCK II_to_III")
 print("Adding a random line.")
 print("Adding a random line.")
 print("Adding a random line.")
 
-your_III_to_IV = int(time.time()*1000)
+your_III = int(time.time()*1000)
 
 print("BLOCK III_to_IV")
 print("Adding a random line.")
@@ -49,20 +52,43 @@ print("Adding a random line.")
 your_IV = int(time.time()*1000)
 
 
+print("BLOCK IV_to_V")
+print("Adding a random line.")
+print("Adding a random line.")
+print("Adding a random line.")
+print("Adding a random line.")
+
+your_V =  int(time.time()*1000)
+
+print("BLOCK V_to_VI")
+print("Adding a random line.")
+print("Adding a random line.")
+print("Adding a random line.")
+print("Adding a random line.")
+
+
+your_VI =  int(time.time()*1000)
+
 print(
-    "\n> Delay block I to II : {} ms".format(your_II_to_III - your_I_to_II),
-    "\n> Delay block II to III : {} ms".format(your_III_to_IV - your_II_to_III),
-    "\n> Delay block III to IV : {} ms".format(your_IV - your_III_to_IV)
+    "\n> Delay block I to II : {} ms".format(your_II - your_I),
+    "\n> Delay block II to III : {} ms".format(your_III - your_II),
+    "\n> Delay block III to IV : {} ms".format(your_IV - your_III),
+    "\n> Delay block IV to V : {} ms".format(your_V - your_IV),
+    "\n> Delay block V to VI : {} ms".format(your_VI - your_V)
 )
 
 data = list()
-data.append(your_II_to_III - your_I_to_II)
-data.append(your_III_to_IV - your_II_to_III)
-data.append(your_IV - your_III_to_IV)
-print(data)
+data.append(your_II - your_I)
+data.append(your_III - your_II)
+data.append(your_IV - your_III)
+data.append(your_V - your_IV)
+data.append(your_VI - your_V)
+
+print("Your data : {}".format(data))
 
 plt.plot(data, 'b--')
 plt.ylabel('Time (ms)')
-plt.axis([0,3,0,48])
-plt.title('Block I-II, II-III, III-IV')
+plt.axis([0,4,0,48])
+plt.title('Block I-II, II-III, III-IV,IV-V, V-IV')
+plt.grid()
 plt.show()
